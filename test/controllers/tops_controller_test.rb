8b40ed8db1a989be457104fbd7873c526bb518d9
maru8181 <stop_1_supasupa@yahoo.co.jp>
2020-01-13 15:24:43 +0900

    rubocop整形

--- test/controllers/tops_controller_test.rb
+++ test/controllers/tops_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class TopsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get root_path
     assert_response :success
   end
-
 end
--- test/controllers/tweets_controller_test.rb
+++ test/controllers/tweets_controller_test.rb
@@ -5,17 +5,17 @@ class TweetsControllerTest < ActionDispatch::IntegrationTest
     @tweet = tweets(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get tweets_path
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_tweet_path
     assert_response :success
   end
 
-  test "should create tweet" do
+  test 'should create tweet' do
     assert_difference('Tweet.count') do
       post tweets_url, params: { tweet: { golf_id: @tweet.golf_id, point_id: @tweet.point_id, text: @tweet.text } }
     end
@@ -23,22 +23,22 @@ class TweetsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to tweet_url(Tweet.last)
   end
 
-  test "should show tweet" do
+  test 'should show tweet' do
     get tweet_path(@tweet)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_tweet_path(@tweet)
     assert_response :success
   end
 
-  test "should update tweet" do
+  test 'should update tweet' do
     patch tweet_path(@tweet), params: { tweet: { golf_id: @tweet.golf_id, point_id: @tweet.point_id, text: @tweet.text } }
     assert_redirected_to tweet_url(@tweet)
   end
 
-  test "should destroy tweet" do
+  test 'should destroy tweet' do
     assert_difference('Tweet.count', -1) do
       delete tweet_path(@tweet)
     end
