a5292cc553a624232013602720c43f73af43c8f3
maru8181 <stop_1_supasupa@yahoo.co.jp>
2020-01-13 22:33:15 +0900

    記事投稿編集,統合テスト追加

---
+++ test/integration/tweets_edit_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class TweetsEditTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @tweet = tweets(:tweet1)
+  end
+
+  test 'edit invalid' do
+    get edit_tweet_path(@tweet)
+    patch tweet_path(@tweet), params: { tweet: { golf_id: nil, point_id: nil, text: 'a'*10001 } }
+    assert_template :edit
+  end
+
+  test 'edit valid' do
+    get edit_tweet_path(@tweet)
+    patch tweet_path(@tweet), params: { tweet: { golf_id: @tweet.golf_id, point_id: @tweet.point_id, text: @tweet.text } }
+    assert_redirected_to tweet_url(@tweet)
+    follow_redirect!
+    assert_template 'tweets/show'
+  end
+
+end
