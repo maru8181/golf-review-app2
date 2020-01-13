154fb26a47bfb46f0e9b56d635c2741934c655be
maru8181 <stop_1_supasupa@yahoo.co.jp>
2020-01-13 12:45:02 +0900

    投稿機能修正

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -14,10 +14,15 @@ class TweetsController < ApplicationController
   # GET /tweets/new
   def new
     @tweet = Tweet.new
+    @golves = Golf.all
+    @points = Point.all
   end
 
   # GET /tweets/1/edit
-  def edit; end
+  def edit
+    @golves = Golf.all
+    @points = Point.all
+  end
 
   # POST /tweets
   # POST /tweets.json
@@ -26,7 +31,7 @@ class TweetsController < ApplicationController
 
     respond_to do |format|
       if @tweet.save
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
+        format.html { redirect_to @tweet, notice: '投稿しました' }
         format.json { render :show, status: :created, location: @tweet }
       else
         format.html { render :new }
@@ -40,7 +45,7 @@ class TweetsController < ApplicationController
   def update
     respond_to do |format|
       if @tweet.update(tweet_params)
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully updated.' }
+        format.html { redirect_to @tweet, notice: '編集しました' }
         format.json { render :show, status: :ok, location: @tweet }
       else
         format.html { render :edit }
@@ -54,7 +59,7 @@ class TweetsController < ApplicationController
   def destroy
     @tweet.destroy
     respond_to do |format|
-      format.html { redirect_to tweets_url, notice: 'Tweet was successfully destroyed.' }
+      format.html { redirect_to tweets_url, notice: '削除しました' }
       format.json { head :no_content }
     end
   end
