c2ee3bef3013aa0cc46df0cff184de480a8ee117
maru8181 <stop_1_supasupa@yahoo.co.jp>
2020-01-13 22:08:54 +0900

    tweetモデル単体テスト追加

--- test/models/tweet_test.rb
+++ test/models/tweet_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class TweetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @tweet = tweets(:tweet1)
+  end
+
+  test 'should be valid' do
+    assert @tweet.valid?
+  end
+
+  test 'golf_id should be presence' do
+    @tweet.golf_id = ''
+    assert_not @tweet.valid?
+  end
+
+  test 'point_id should be presence' do
+    @tweet.point_id = ''
+    assert_not @tweet.valid?
+  end
+
+  test 'text should not be too long' do
+    @tweet.text = 'a' * 10_001
+    assert_not @tweet.valid?
+  end
 end
