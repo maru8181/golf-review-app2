5ec8822d77d2a3a0ee25f283c26465b22f4f8885
maru8181 <stop_1_supasupa@yahoo.co.jp>
2020-01-15 10:55:05 +0900

    devise日本語化

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_14_064321) do
+ActiveRecord::Schema.define(version: 2020_01_15_014948) do
 
   create_table "golves", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_01_14_064321) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
