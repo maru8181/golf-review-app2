9b46a532bc4a0bc93de5b2679d1ff430cd96fa79
maru8181 <stop_1_supasupa@yahoo.co.jp>
2020-01-15 12:00:04 +0900

    ユーザー登録情報追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+ private
+      def sign_in_required
+          redirect_to new_user_session_url unless user_signed_in?
+      end
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    end
+
 end
