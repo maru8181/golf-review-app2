336629d6b85aa539571dd7fe0d14938586c35342
maru8181 <stop_1_supasupa@yahoo.co.jp>
2020-01-19 17:07:01 +0900

    Relationshipモデル追加,userモデル関連付け,モデルテスト作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  has_many :tweets
+  has_many :tweets, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
