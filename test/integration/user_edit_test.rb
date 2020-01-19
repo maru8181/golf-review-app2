e956473826094611757d54941832d74c7ae46ba5
maru8181 <stop_1_supasupa@yahoo.co.jp>
2020-01-19 13:41:17 +0900

    ユーザーページに投稿一覧表示,rubocop整形

--- test/integration/user_edit_test.rb
+++ test/integration/user_edit_test.rb
@@ -14,9 +14,9 @@ class UserEditTest < ActionDispatch::IntegrationTest
 
   # test 'edit valid' do
   #   get edit_user_registration_path(@user)
-    # patch user_registration_path(@user), params: { user: { username: 'example0', email: 'hoge@example.co.jp', password: '', password_confirmation: '', current_password: 'password' } }
-    # assert_redirected_to root_url
-    # follow_redirect!
-    # assert_template 'tops/index'
+  # patch user_registration_path(@user), params: { user: { username: 'example0', email: 'hoge@example.co.jp', password: '', password_confirmation: '', current_password: 'password' } }
+  # assert_redirected_to root_url
+  # follow_redirect!
+  # assert_template 'tops/index'
   # end
 end
