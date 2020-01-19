6b060cee2bda00bfc392ae06a8b35d2c5f317fc2
maru8181 <stop_1_supasupa@yahoo.co.jp>
2020-01-20 00:16:50 +0900

    フォロー,フォロワー機能追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@ Point.find_or_create_by(valuation: 3)
 Point.find_or_create_by(valuation: 4)
 Point.find_or_create_by(valuation: 5)
 
-User.create!(username: 'example', email: 'example@yahoo.co.jp', password: 'hogehoge', password_confirmation: 'hogehoge', confirmed_at: Time.now - 100)
-User.create!(username: 'example00', email: 'example123@yahoo.co.jp', password: 'fugafuga', password_confirmation: 'fugafuga', confirmed_at: Time.now - 100)
+# User.create!(username: 'example', email: 'example@yahoo.co.jp', password: 'hogehoge', password_confirmation: 'hogehoge', confirmed_at: Time.now - 100)
+# User.create!(username: 'example00', email: 'example123@yahoo.co.jp', password: 'fugafuga', password_confirmation: 'fugafuga', confirmed_at: Time.now - 100)
