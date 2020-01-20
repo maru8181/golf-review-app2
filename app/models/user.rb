933350a2ee897e7f5ff94d19bd5d45128cfc36a0
maru8181 <stop_1_supasupa@yahoo.co.jp>
2020-01-20 12:32:42 +0900

    フォロー,フォロワー機能修正,統合テスト修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,10 @@ class User < ApplicationRecord
   def following?(other_user)
     following.include?(other_user)
   end
+
+  # ユーザーのステータスフィードを返す
+  # def feed
+  #   following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+  #   Tweet.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+  # end
 end
