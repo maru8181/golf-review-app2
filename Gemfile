75a4c39238f01127d19bbdcf6b0727c7ae8b520c
maru8181 <stop_1_supasupa@yahoo.co.jp>
2020-01-20 20:10:50 +0900

    ページネーション設定

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'bootstrap-sass', '3.4.1'
 gem 'devise'
 gem 'devise-i18n'
 gem 'devise-i18n-views'
+gem 'will_paginate', '3.1.7'
+gem 'bootstrap-will_paginate','1.0.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.1'
 # Use sqlite3 as the database for Active Record
--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,20 +3,20 @@ class MembersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @tweets = Tweet.where(user_id: params[:id])
+    @tweets = Tweet.where(user_id: params[:id]).paginate(page: params[:page])
   end
 
   def following
     @title = 'Following'
     @user  = User.find(params[:id])
-    @users = @user.following.all
+    @users = @user.following.all.paginate(page: params[:page])
     render 'show_follow'
   end
 
   def followers
     @title = 'Followers'
     @user  = User.find(params[:id])
-    @users = @user.followers.all
+    @users = @user.followers.all.paginate(page: params[:page])
     render 'show_follow'
   end
 end
