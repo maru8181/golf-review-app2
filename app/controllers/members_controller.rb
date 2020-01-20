16e86de50fa7d786e885ef80c4906f6e485cf99a
maru8181 <stop_1_supasupa@yahoo.co.jp>
2020-01-20 11:36:39 +0900

    route変更,テスト記載

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,8 +1,22 @@
 class MembersController < ApplicationController
-  before_action :sign_in_required, only: [:show]
+  before_action :sign_in_required, only: %i[show following followers]
 
   def show
     @user = User.find(params[:id])
     @tweets = Tweet.where(user_id: params[:id])
+  end
+
+  def following
+    @title = 'Following'
+    @user  = User.find(params[:id])
+    @users = @user.following.all
+    render 'show_follow'
+  end
+
+  def followers
+    @title = 'Followers'
+    @user  = User.find(params[:id])
+    @users = @user.followers.all
+    render 'show_follow'
   end
 end
