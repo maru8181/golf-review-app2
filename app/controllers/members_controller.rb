7e5f167931f1e3881d7b1a4c91cdf02b09197725
maru8181 <stop_1_supasupa@yahoo.co.jp>
2020-01-21 15:59:06 +0900

    ツイート表示機能修正

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,12 +1,6 @@
 class MembersController < ApplicationController
   before_action :sign_in_required, only: %i[show following followers]
 
-  def index
-    @user = User.find(params[:member_id])
-    following_ids = 'SELECT followed_id FROM relationships WHERE follower_id = :user_id'
-    @tweets = Tweet.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: :member_id)
-  end
-
   def show
     @user = User.find(params[:id])
     @tweets = Tweet.where(user_id: params[:id]).paginate(page: params[:page])
