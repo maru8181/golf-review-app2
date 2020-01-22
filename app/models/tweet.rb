63faf711b62ac370aba5c218f00e46678119c87b
maru8181 <stop_1_supasupa@yahoo.co.jp>
2020-01-22 14:18:50 +0900

    いいね機能追加

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,7 +2,13 @@ class Tweet < ApplicationRecord
   belongs_to :golf
   belongs_to :point
   belongs_to :user
+  has_many :likes, dependent: :destroy
+
   validates :golf_id, presence: true
   validates :point_id, presence: true
   validates :text, length: { minimum: 0, maximum: 10_000 }
+
+  def like_user(user_id)
+    likes.find_by(user_id: user_id)
+  end
 end
