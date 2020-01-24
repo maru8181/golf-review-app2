0172f65b1d64841aaa3a68fe09ac9ae5dab30fd0
maru8181 <stop_1_supasupa@yahoo.co.jp>
2020-01-24 15:08:19 +0900

    管理ユーザー,ユーザー削除権限付加

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,10 @@
 class MembersController < ApplicationController
   before_action :sign_in_required, only: %i[show following followers]
+  before_action :admin_required, only: %i[index delete]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
     @user = User.find(params[:id])
@@ -18,5 +23,20 @@ class MembersController < ApplicationController
     @user  = User.find(params[:id])
     @users = @user.followers.all.paginate(page: params[:page])
     render 'show_follow'
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.admin?
+      flash[:admin_error] = '管理人は削除できません'
+      redirect_to members_url
+    # elsif @user.email = 'guest@example.com'
+    #   flash[:guest_error] = 'ゲストユーザーは削除できません'
+    #   redirect_to members_url
+    else
+      @user.destroy
+      flash[:delete_success] = '削除しました'
+      redirect_to members_url
+    end
   end
 end
