b3de50232c6c4d142b08146b93e9b5e884bcc045
maru8181 <stop_1_supasupa@yahoo.co.jp>
2020-01-24 19:13:05 +0900

    ゴルフ場一覧,ゴルフ場詳細ページ作成,リンク作成

---
+++ app/controllers/golves_controller.rb
@@ -0,0 +1,12 @@
+class GolvesController < ApplicationController
+  def index
+    @q = Golf.ransack(params[:q])
+    @golves = @q.result(distinct: true)
+  end
+
+  def show
+    @golf = Golf.find(params[:id])
+    @tweets = Tweet.where(golf_id: @golf.id)
+  end
+
+end
