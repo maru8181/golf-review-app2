6d9c029d438dda0e521f3eb9407de05824da7915
maru8181 <stop_1_supasupa@yahoo.co.jp>
2020-01-24 13:26:46 +0900

    管理ユーザー機能追加,管理ユーザー記事削除機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_23_070230) do
+ActiveRecord::Schema.define(version: 2020_01_24_035415) do
 
   create_table "golves", force: :cascade do |t|
     t.string "name"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2020_01_23_070230) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
